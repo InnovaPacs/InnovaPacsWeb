1501e994e54d8fa347c5be670fbcb624af0aa4f7
bautistaj <bautistaj20@outlook.com>
2021-08-08 12:26:05 -0500

    (fix) loading js

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -6,15 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./layout.component.sass']
 })
 export class LayoutComponent implements OnInit {
+  public customScript = document.querySelector('#custom-script');
   public hideMenu = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    const url = `./assets/js/custom.js`;
+    this.customScript.setAttribute('src',url);
   }
 
   handleMenuButton() {
-    
     this.hideMenu = !this.hideMenu;
   }
 }
