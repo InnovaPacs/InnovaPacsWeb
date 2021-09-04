a1c294db8c2bdbbb4af27bcc290446aff8a37ccf
bautistaj <bautistaj20@outlook.com>
2021-09-04 11:49:55 -0500

    (config) to deply

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+echo "Iniciando despliegue"
+#git pull
+
+CID=$(docker ps -aqf "name=innova-pacs-web-server")
+echo $CID
+
+echo "Detendiendo contenedor"
+docker stop $CID
+
+echo "Borrando contenedor"
+docker rm $CID
+
+echo "Borrando imagen"
+docker rmi innova-pacs-web-server:v1
+docker build -t innova-pacs-web-server:v1 .
+docker run -p 4200:4200 --network dcm4chee_default --name innova-pacs-web-server -d innova-pacs-web-server:v1
+
+echo "Terminando despliegue"
