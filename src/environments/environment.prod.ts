b6370bf54742ec448d2fba0cb19568c065602a3e
camposb <jcampos@ontop.ai>
2022-05-07 15:25:02 -0500

    (config) new prd ip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  API: 'http://66.94.96.116:8081/innova-pacs/',
+  API: 'http://192.168.0.3:8081/innova-pacs/',
   //DCM4CHEE_API: 'https://66.94.96.116:8443/dcm4chee-arc/',
   CLIENT_ID: 'InnovaPacs',
   CLIENT_SECRET: 'InnovaPacs2021',
   GRANT_TYPE: 'password',
-  HOST: '66.94.96.116'
+  HOST: '192.168.0.3'
 };
