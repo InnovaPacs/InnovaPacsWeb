095bf8c142aebf36ff51b008c688823c06244187
bautistaj <jcampos@ontop.ai>
2022-08-20 20:17:56 -0500

    (config) client 1

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 echo "Iniciando despliegue"
 #git pull
 
-CID=$(docker ps -aqf "name=innova-pacs-web-server")
+CID=$(docker ps -aqf "name=innova-pacs-web-server-c1")
 echo $CID
 
 echo "Detendiendo contenedor"
@@ -11,8 +11,8 @@ echo "Borrando contenedor"
 docker rm $CID
 
 echo "Borrando imagen"
-docker rmi innova-pacs-web-server:v1
-docker build -t innova-pacs-web-server:v1 .
-docker run -p 4210:4200 --network dcm4chee_default --name innova-pacs-web-server -d innova-pacs-web-server:v1
+docker rmi innova-pacs-web-server-c1:v1
+docker build -t innova-pacs-web-server-c1:v1 .
+docker run -p 4210:4200 --network dcm4chee_default --name innova-pacs-web-server-c1 -d innova-pacs-web-server-c1:v1
 
 echo "Terminando despliegue"
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API: 'http://66.94.96.116:8081/innova-pacs/',
+  API: 'http://66.94.96.116:8085/innova-pacs/',
   CLIENT_ID: 'InnovaPacs',
   CLIENT_SECRET: 'InnovaPacs2021',
   GRANT_TYPE: 'password',
