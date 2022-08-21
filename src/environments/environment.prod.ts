859a4d7647483136c16425b82690f7ad5000c5f0
bautistaj <jcampos@ontop.ai>
2022-08-20 20:29:24 -0500

    (config) port

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API: 'http://66.94.96.116:8085/innova-pacs/',
+  API: 'http://66.94.96.116:8090/innova-pacs/',
   CLIENT_ID: 'InnovaPacs',
   CLIENT_SECRET: 'InnovaPacs2021',
   GRANT_TYPE: 'password',
