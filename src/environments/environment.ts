cec31020112f5ecb2a26514751c8ecc712b27904
bautistaj <jcampos@ontop.ai>
2022-11-06 23:42:56 -0600

    configure demo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API: 'http://66.94.96.116:8081/innova-pacs/',
+  API: 'http://66.94.96.116:8090/innova-pacs/',
   CLIENT_ID: 'InnovaPacs',
   CLIENT_SECRET: 'InnovaPacs2021',
   GRANT_TYPE: 'password',
