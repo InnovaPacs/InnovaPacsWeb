62ad82b239f7e9077e9a29736981cc746060849c
bautistaj <jcampos@ontop.ai>
2023-03-21 20:11:55 -0600

    Add https urls

--- src/app/admin/components/full-studies/full-studies.component.ts
+++ src/app/admin/components/full-studies/full-studies.component.ts
@@ -86,6 +86,7 @@ export class FullStudiesComponent implements OnInit {
 
   public viewStudy(studyIuid: string){
     if(studyIuid){
+      console.log(`https://${this.host}/weasis-pacs-connector/weasis?studyUID=${studyIuid}&cdb`);
       window.open(`https://${this.host}/weasis-pacs-connector/weasis?studyUID=${studyIuid}&cdb`, '_blank');
     }
   }
