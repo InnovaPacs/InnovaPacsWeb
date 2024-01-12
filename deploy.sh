3349535960977b5b25607217b44cc0cc8fcfe05b
Jose Luis Campos Bautista <bautistaj@Joses-MacBook-Pro.local>
2024-01-12 07:53:38 -0600

    Configure dev env

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ docker rm $CID
 echo "Borrando imagen"
 docker rmi innova-pacs-web-demo-server:v1
 docker build -t innova-pacs-web-demo-server:v1 .
-docker run -p 4210:4200 --network dcm4chee_default --name innova-pacs-web-demo-server -d innova-pacs-web-demo-server:v1
+docker run -p 4210:4200 --network dcm4chee --name innova-pacs-web-demo-server -d innova-pacs-web-demo-server:v1
 
 echo "Terminando despliegue"
